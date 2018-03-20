87ade353627e622478423cae3f2d05eb201d675d
Tyler <tyler@nanoleaf.me>
2018-03-19 22:41:46 -0400

    added grid builder

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,8 +1,27 @@
 defmodule Identicon do
   def main(input) do
-    input |> hash_input |> pick_color
+    input 
+    |> hash_input 
+    |> pick_color 
+    |> build_grid
+  end
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+    grid = 
+    hex 
+      |> Enum.chunk(3) 
+      |> Enum.map(&mirror_row/1) 
+      |> List.flatten 
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+
   end
 
+  def mirror_row(row) do
+    [f,s | _tail] = row
+    row ++ [s,f]
+  end
   def pick_color(image) do
     %Identicon.Image{hex: [r,g,b | _tail]} = image
     %Identicon.Image{image | color: {r,g,b}}
